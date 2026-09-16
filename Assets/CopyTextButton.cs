1b0f8366e3c94d228905cc755ab87a295df9403c
Gerardo <gerardogonzalezmartin2004@gmail>
2026-09-16 16:27:46 +0200

    Cambios
    Revisa claise 16/09

---
+++ Assets/CopyTextButton.cs
@@ -0,0 +1,14 @@
+/*
+using UnityEngine;
+using TMPro;
+
+public class CopyTextButton : MonoBehaviour
+{
+   
+    public TMP_Text joinCodeText;
+    public void CopyTextFromTextField()
+    {
+        GUIUutility.systemCopyBuffer = joinCodeText.text;
+    }
+}
+*/
